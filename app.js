1c6e64404c4169fa4f0577e445d4d0453566bf1e
ChidiChuks <chukschiboy@gmail.com>
2020-04-11 17:35:18 +0100

    featuring the total expenses

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const amountInput = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 const expensesList = document.querySelector('#expenses-list');
+const totalExpensesOutput = document.querySelector('#total-expenses');
+
+let totalExpenses = 0;
 
 const clear = () => {
     reasonInput.value = '';
@@ -21,6 +24,8 @@ confirmBtn.addEventListener('click', () => {
 
     expensesList.appendChild(newItem);
 
+    totalExpenses += +enteredAmount;
+    totalExpensesOutput.textContent = totalExpenses;
     clear();
 });
 
