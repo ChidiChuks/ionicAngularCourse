773772dc4351e68cdccd89c7cd8cf96c902f3eee
ChidiChuks <chukschiboy@gmail.com>
2020-04-11 22:21:45 +0100

    uploading the my course ratings project dev

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 const expensesList = document.querySelector('#expenses-list');
 const totalExpensesOutput = document.querySelector('#total-expenses');
+const alertCtrl = document.querySelector('ion-alert-controller');
 
 let totalExpenses = 0;
 
@@ -16,7 +17,28 @@ confirmBtn.addEventListener('click', () => {
     const enteredReason = reasonInput.value;
     const enteredAmount = amountInput.value;
 
-    if (enteredReason.trim().length <= 0 || enteredAmount <= 0 || enteredAmount.trim().length <= 0) {
+    if (
+        enteredReason.trim().length <= 0 || 
+        enteredAmount <= 0 || 
+        enteredAmount.trim().length <= 0
+    ) {
+        alertCtrl.create({
+            message: 'Please enter valid reason and amount!', 
+            header: 'Invalid inputs', 
+            buttons: ['Okay']
+    }).then(alertElement => {
+        alertElement.present();
+    });
+        // function presentAlert() {
+        //     const alert = document.createElement('ion-alert');
+        //     alert.header = 'Invalid inputs';
+        //     alert.subHeader = ''; // Subtitle
+        //     alert.message = 'Please enter valid reason and amount!';
+        //     alert.buttons = ['OKay'];
+        
+        //     document.body.appendChild(alert);
+        //     return alert.present();
+        // }
         return;
     }
     const newItem = document.createElement('ion-item');
