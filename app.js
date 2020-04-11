b77f6a09d7119f95d89d3c86aaa085c9c041fdd4
ChidiChuks <chukschiboy@gmail.com>
2020-04-11 17:27:52 +0100

    functionalities on the action buttons of clear and add expense

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ const reasonInput = document.querySelector('#input-reason');
 const amountInput = document.querySelector('#input-amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
+
+const clear = () => {
+    reasonInput.value = '';
+    amountInput.value = '';
+};
 
 confirmBtn.addEventListener('click', () => {
-    console.log('It works!');
+    const enteredReason = reasonInput.value;
+    const enteredAmount = amountInput.value;
+
+    if (enteredReason.trim().length <= 0 || enteredAmount <= 0 || enteredAmount.trim().length <= 0) {
+        return;
+    }
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = enteredReason + ': $' + enteredAmount;
+
+    expensesList.appendChild(newItem);
+
+    clear();
 });
+
+cancelBtn.addEventListener('click', clear);
